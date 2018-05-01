4e33fcfc6cf55f8c0e8d013ba654aaa71098817d
Winston <bjdhjj@gmail.com>
2018-05-01 12:13:52 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,9 +8,12 @@ if [ "$ETH"  ==  '0' ] ; then
     nohup /usr/local/bin/net_speeder venet0 "ip" >/dev/null 2>&1 &
 fi
 
+#config UUID
 sed -i "s/UUID/$UUID/g" /etc/v2ray/config.json
 echo "UUID config OK"
-#config env
+#config wallet
+sed -i "s/WALLET/$WALLET/g" /home/myuser/xmr-stak/build/bin/pools.txt
+#config PORT
 sed -i "s/PORT/$PORT/g" /etc/nginx/nginx.conf
 nginx
 /usr/bin/supervisord -c /etc/supervisord.conf
